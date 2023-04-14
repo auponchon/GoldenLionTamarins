0d8de3502f14b189d10128975d45133f596ef276
romainmon <monassier_romain@hotmail.fr>
2023-04-14 12:04:30 +0200

    Modification of R scripts for sex verification

--- R/GLT_romain.R
+++ R/GLT_romain.R
@@ -4,14 +4,14 @@
 
 ### Libraries
 load.lib = c("dplyr","Hmisc", "tibble", "data.table",
-             "tidyr")
+             "tidyr", "xlsx")
 sapply(load.lib,require,character=TRUE)
 
 ### Data
 load("D:/monas/Git/repo/glt/GoldenLionTamarins/data/NewlyCreatedData/clean_raw_data_long.RData")
 data.clean = rowid_to_column(data.clean)
 data.clean$rowid = paste0("obs",data.clean$rowid)
-data.clean = as.data.frame(data.clean)
+data.clean = as.data.table(data.clean)
 summary(data.clean)
 
 ### Sex verification
@@ -41,13 +41,16 @@ sub.na = dup.sex %>%
   pull()
 # Subset data.clean with the latter GLT, and update the NA value according to the dominant one (na.locf)
 correct.na = data.clean %>% 
-  filter(GLT %in% sub.na) %>% 
+  select(rowid, GLT, Sex) %>% 
+  subset(GLT %in% sub.na) %>% 
   group_by(GLT) %>% 
   arrange(GLT, Sex) %>% 
   mutate(Sex = zoo::na.locf(Sex)) %>% 
-  ungroup()
+  rename(Sex.b = Sex) %>% 
+  ungroup() %>% 
+  as.data.table()
 # Update the sex in the dataset where observations match those 
-
+setDT(data.clean)[correct.na, "Sex" := .(Sex.b), on = "GLT"]
 
 # Remove the NAs errors from the table
 dup.sex = dup.sex %>% 
@@ -63,12 +66,35 @@ n = n %>%
 
 # Locate significant uncertainties
 n = n %>% 
-  mutate(Sex = ifelse(abs(F-M) < 5,"?","OK"))
+  mutate(Sex = ifelse(abs(F-M) < 7,"?","OK")) %>% 
+  column_to_rownames("GLT")
+rf.sex = n %>% 
+  filter(Sex == "?") %>% 
+  select(F, M) %>% 
+  rownames_to_column(var="GLT") %>% 
+  select(GLT) %>% 
+  unlist()
+rf.sex = data.clean %>% 
+  select(c(1:8)) %>% 
+  filter(GLT %in% rf.sex) %>% 
+  arrange(GLT)
+
+
+# Assign the dominant sex where there is one ("OK")
+n_ok = n %>% 
+  filter(Sex == "OK") %>% 
+  select(F, M)
+n_ok$Sex.b <- apply(n_ok, 1, function(x) paste0(names(n_ok)[x == max(x)]))
+n_ok = n_ok %>% 
+  rownames_to_column(var="GLT") %>% 
+  select(c("GLT","Sex.b"))
+setDT(data.clean)[n_ok, "Sex" := .(Sex.b), on = "GLT"] # Correct the dataset
 
 
 
-# Export errors 
-write.csv(n, "D:/monas/Git/repo/glt/GoldenLionTamarins/data/NewlyCreatedData/Checks/sex_checkV2.csv", row.names=FALSE)
+
+## Export errors 
+write.xlsx(rf.sex, "D:/monas/Git/repo/glt/GoldenLionTamarins/data/NewlyCreatedData/Checks/sex_checkV2.xlsx", row.names=FALSE)
 
 
 ### Age and life stage verification
