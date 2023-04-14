ca975fa31c3839d5b37ed1cec6db27a17bbaccb0
romainmon <monassier_romain@hotmail.fr>
2023-04-14 16:59:23 +0200

    Sex and age processing

--- R/GLT_romain.R
+++ R/GLT_romain.R
@@ -3,8 +3,8 @@
 ### Date: 2023
 
 ### Libraries
-load.lib = c("dplyr","Hmisc", "tibble", "data.table",
-             "tidyr", "xlsx")
+load.lib = c("dplyr","tibble", "data.table",
+             "tidyr", "xlsx", "zoo")
 sapply(load.lib,require,character=TRUE)
 
 ### Data
@@ -15,7 +15,7 @@ data.clean = as.data.table(data.clean)
 summary(data.clean)
 
 ### Sex verification
-# Update case mistakes
+# Update typing errors
 data.clean = data.clean %>% 
   mutate(Sex = ifelse(Sex == "m", "M",
                       Sex))
@@ -91,10 +91,86 @@ n_ok = n_ok %>%
 setDT(data.clean)[n_ok, "Sex" := .(Sex.b), on = "GLT"] # Correct the dataset
 
 
-
-
 ## Export errors 
 write.xlsx(rf.sex, "D:/monas/Git/repo/glt/GoldenLionTamarins/data/NewlyCreatedData/Checks/sex_checkV2.xlsx", row.names=FALSE)
 
 
 ### Age and life stage verification
+
+# Update typing errors
+data.clean %>% 
+  group_by(Birth) %>%
+  summarise(no_rows = length(Birth)) %>% 
+  print(n=300)
+data.clean = data.clean %>% 
+  mutate(Birth = ifelse(Birth == "'" | Birth == "-" | Birth == "?" | Birth == "??" | Birth == "died"
+                        | Birth == "42614" | Birth == "42615" | Birth == "FA20" | Birth == "RC1"
+                        | Birth == "RC2" | Birth == "RC3", NA,
+                        ifelse(Birth == "10/ 21", "10/21",
+                               ifelse(Birth == "1015", "10/15",
+                                      Birth))))
+
+# Conversion of Birth variable to Date format
+n = data.clean %>% 
+  select(c(rowid, DateObs, GLT, Birth, Idade)) %>% 
+  filter(!is.na(Birth))
+n$Birth = paste0('01/', n$Birth)
+n$Birth = n$Birth %>% as.Date(n$Birth, format="%d/%m/%y")
+IsDate <- function(mydate, date.format = "%d/%m/%y") {
+  tryCatch(!is.na(as.Date(mydate, date.format)),  
+           error = function(err) {FALSE})  
+}
+
+IsDate(n$DateObs)
+
+## Duplicated/equivocal birth dates
+dup.birth = n %>% 
+  select(GLT, Birth) %>% 
+  group_by(GLT) %>%
+  filter(n_distinct(Birth) > 1) %>%
+  ungroup()
+dup.birth = dup.birth %>% 
+  group_by(GLT, Birth) %>% 
+  summarise(n=n())
+dup.birth = dup.birth %>% 
+  group_by(GLT) %>% 
+  mutate(Births = paste0(Birth, collapse = " or ")) %>% 
+  distinct(GLT, Births) %>% 
+  ungroup()
+dup.birth = dup.birth %>% 
+  filter(GLT != "?") %>% 
+  separate(Births, c("opt1", "opt2", "opt3", "opt4", "opt5"), " or ")
+
+# Create subsets according to the number of different birth dates
+# Two different dates
+dup.birth2 = dup.birth %>% 
+  filter(is.na(opt3)) %>% 
+  select(1:3)
+dup.birth2$opt1 = as.Date(x = dup.birth2$opt1, format="%Y-%m-%d")
+dup.birth2$opt2 = as.Date(x = dup.birth2$opt2, format="%Y-%m-%d")
+dup.birth2 = dup.birth2 %>% 
+  mutate(diffBirth = abs(difftime(opt1, opt2, units="days")))
+# Three different dates
+dup.birth3 = dup.birth %>% 
+  filter(is.na(opt4) & !is.na(opt3)) %>% 
+  select(1:4)
+dup.birth3$opt1 = as.Date(x = dup.birth3$opt1, format="%Y-%m-%d")
+dup.birth3$opt2 = as.Date(x = dup.birth3$opt2, format="%Y-%m-%d")
+dup.birth3$opt3 = as.Date(x = dup.birth3$opt3, format="%Y-%m-%d")
+# Five different dates
+dup.birth5 = dup.birth %>% 
+  filter(!is.na(opt5))
+dup.birth5$opt1 = as.Date(x = dup.birth5$opt1, format="%Y-%m-%d")
+dup.birth5$opt2 = as.Date(x = dup.birth5$opt2, format="%Y-%m-%d")
+dup.birth5$opt3 = as.Date(x = dup.birth5$opt3, format="%Y-%m-%d")
+dup.birth5$opt4 = as.Date(x = dup.birth5$opt4, format="%Y-%m-%d")
+dup.birth5$opt5 = as.Date(x = dup.birth5$opt5, format="%Y-%m-%d")
+
+# Implementing the Idade column
+n = n %>% 
+  mutate(Idade2 = ifelse(difftime(n$DateObs, n$Birth, units="days") <= 365, "JU",
+                         ifelse(difftime(n$DateObs, n$Birth, units="days") >365 & difftime(n$DateObs, n$Birth, units="days")<=1095, "SA",
+                                ifelse(difftime(n$DateObs, n$Birth, units="days") > 1095, "AD",
+                                       Idade))))
+data.clean = data.clean %>% 
+  left_join(select(n,c(rowid,Idade2)))
