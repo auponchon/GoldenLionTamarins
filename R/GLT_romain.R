ed7203497071759aaf8c56e531b0d7885246363a
romainmon <monassier_romain@hotmail.fr>
2023-04-18 17:57:23 +0200

    Birth dates errors identification

--- R/GLT_romain.R
+++ R/GLT_romain.R
@@ -95,75 +95,113 @@ setDT(data.clean)[n_ok, "SexOK" := .(Sex.b), on = "GLT"] # Correct the dataset
 
 # Manually select uncertain rows based on visual inspection
 rf.sex = data.clean %>% 
-  filter(GLT == "1292" | GLT == "1294" | GLT == "CM13" | GLT == "LA15" | GLT == "MP33" | 
-           GLT == "RL11" | GLT == "RL14" | GLT == "RL2" | GLT == "RT2" | GLT == "RV3" | 
-           GLT == "RZ9" | GLT == "SF2" | GLT == "SF3" | GLT == "SF4" | GLT == "SK19" | 
-           GLT == "ST14" | GLT == "ST3" | GLT == "ST40" | GLT == "RL13")
+  filter(GLT == "RL11" | GLT == "RL13" | GLT == "SF3" | GLT == "SF4" | GLT == "SK19")
 
 ## Export errors 
-write.xlsx(rf.sex, "D:/monas/Git/repo/glt/GoldenLionTamarins/data/NewlyCreatedData/Checks/sex_checkV2.xlsx", row.names=FALSE)
+# write.xlsx(rf.sex, "D:/monas/Git/repo/glt/GoldenLionTamarins/data/NewlyCreatedData/Checks/sex_checkV2.xlsx", row.names=FALSE)
 
 
 
 ### Age and life stage verification
 
 ## Birth Dates from 1989 to 2001
-BirthDates = read_excel("D:/monas/Git/repo/glt/GoldenLionTamarins/data/NewlyCreatedData/Checks/BirthDates.xlsx")
+BirthDates = read_excel("D:/monas/Git/repo/glt/GoldenLionTamarins/data/NewlyCreatedData/Checks/BirthDates.xlsx",
+                        na="NA")
 # Remove duplicated rows (GLT)
 BirthDates = BirthDates[!duplicated(BirthDates$GLT), ]
 # Check typing errors
 data.clean = data.clean %>% 
-  left_join(select(BirthDates, c(1:3,5)))
+  left_join(select(BirthDates, c(1:2,5)))
 data.clean %>% 
-  group_by(BirthDMY) %>%
-  summarise(no_rows = length(Birth)) %>% 
+  group_by(Birth_VR) %>%
+  summarise(no_rows = length(Birth_VR)) %>% 
   print(n=200)
-# Conversion of Birth variable to Date format
-n = data.clean %>% 
-  select(c(rowid, DateObs, GLT, BirthDMY, Idade)) %>% 
-  filter(!is.na(BirthDMY))
-n$BirthDMY = as.Date(n$BirthDMY, format="%d/%m/%y")
-
+# Filter NA data
+n1 = data.clean %>%
+  select(c(rowid, DateObs, GLT, Tattoo, Birth_VR, Idade)) %>%
+  filter(!is.na(Birth_VR) & Birth_VR != "?")
+n1$Birth_VR = as.Date(n1$Birth_VR, format="%d/%m/%y")
 
 
 ## Birth dates post-2001
-# Update typing errors for the Birth field
-# data.clean %>% 
-#   group_by(Birth) %>%
-#   summarise(no_rows = length(Birth)) %>% 
-#   print(n=300)
-# data.clean = data.clean %>% 
-#   mutate(Birth = ifelse(Birth == "'" | Birth == "-" | Birth == "?" | Birth == "??" | Birth == "died"
-#                         | Birth == "42614" | Birth == "42615" | Birth == "FA20" | Birth == "RC1"
-#                         | Birth == "RC2" | Birth == "RC3", NA,
-#                         ifelse(Birth == "10/ 21", "10/21",
-#                                ifelse(Birth == "1015", "10/15",
-#                                       Birth))))
-# n = data.clean %>% 
-#   select(c(rowid, DateObs, GLT, Birth, Idade)) %>% 
-#   filter(!is.na(Birth))
-# n$Birth = paste0('01/', n$Birth)
-# n$Birth = n$Birth %>% as.Date(n$Birth, format="%d/%m/%y")
+# Source : GLT_mov
+data.clean = data.clean %>% 
+  rename(Birth_mov = Birth)
+data.clean %>%
+  group_by(Birth_mov) %>%
+  summarise(no_rows = length(Birth_mov)) %>%
+  print(n=350)
+#Update typing errors for the Birth_mov field
+# data.clean = data.clean %>%
+#   mutate(Birth_mov = ifelse(Birth_mov == "'" | Birth_mov == "-" | Birth_mov == "?" | Birth_mov == "??" | Birth_mov == "died"
+#                         | Birth_mov == "42614" | Birth_mov == "42615" | Birth_mov == "FA20" | Birth_mov == "RC1"
+#                         | Birth_mov == "RC2" | Birth_mov == "RC3", NA,
+#                         ifelse(Birth_mov == "10/ 21", "10/21",
+#                                ifelse(Birth_mov == "1015", "10/15",
+#                                       Birth_mov))))
+n2 = data.clean %>%
+  select(c(rowid, DateObs, GLT, Tattoo, Birth_mov, Idade)) %>%
+  filter(!is.na(Birth_mov))
+# n2$Birth_mov = paste0('01/', n2$Birth_mov)
+n2$Birth_mov = as.Date(n2$Birth_mov, format="%d/%m/%y")
 
-
+# Check date format
 IsDate <- function(mydate, date.format = "%d/%m/%y") {
   tryCatch(!is.na(as.Date(mydate, date.format)),  
            error = function(err) {FALSE})  
 }
-IsDate(n$DateObs)
+IsDate(n1$Birth_VR)
+IsDate(n2$Birth_mov)
+
+# Load GLT movement
+GLT_mov = read_excel("D:/monas/Git/repo/glt/GoldenLionTamarins/data/RawData/GLT_movement.xls",
+                     na="NA")
+GLT_mov = GLT_mov %>% 
+  rename(GLT = Individual) %>% 
+  rename(Tattoo = Tatoo)
+GLT_mov$Estimated.date.of.emigration = as.Date(GLT_mov$Estimated.date.of.emigration, format="%d/%m/%y")
+IsDate(GLT_mov$Estimated.date.of.emigration)
+
+## Merge both subsets of dates
+n = n1 %>% 
+  right_join(n2)
+n = n %>% 
+  left_join(select(GLT_mov,c(GLT, Tattoo, Estimated.date.of.emigration)))
+
+
+## Check Birth dates errors
+# Incoherences between both indicated birth dates
+n = n %>% 
+  mutate(diff.b1.b2 = abs(difftime(Birth_mov, Birth_VR, units="days")))
+n %>%
+  group_by(diff.b1.b2) %>%
+  summarise(no_rows = length(diff.b1.b2)) %>%
+  print(n=60)
+n = n %>% 
+  mutate(Birth = ifelse(!is.na(Birth_mov) & !is.na(Birth_VR), Birth_VR,
+                        Birth_mov))
+n$Birth = as.Date(n$Birth)
+
+# Errors in the movement dataset
+# Comparaison between the estimated.date.of.emigration and the Birth Date
+n = n %>% 
+  mutate(diff.emig.birth = difftime(Estimated.date.of.emigration, Birth, units="days"))
+rf.birthdates = n[grepl("^-", n$diff.emig.birth), ]
+n = n %>% 
+  filter(!grepl("^-", n$diff.emig.birth))
 
 # Duplicated/equivocal birth dates
 dup.birth = n %>% 
-  select(GLT, BirthDMY) %>% 
+  select(GLT, Birth) %>% 
   group_by(GLT) %>%
-  filter(n_distinct(BirthDMY) > 1) %>%
+  filter(n_distinct(Birth) > 1) %>%
   ungroup()
 dup.birth = dup.birth %>% 
-  group_by(GLT, BirthDMY) %>% 
+  group_by(GLT, Birth) %>% 
   summarise(n=n())
 dup.birth = dup.birth %>% 
   group_by(GLT) %>% 
-  mutate(Births = paste0(BirthDMY, collapse = " or ")) %>% 
+  mutate(Births = paste0(Birth, collapse = " or ")) %>% 
   distinct(GLT, Births) %>% 
   ungroup()
 dup.birth = dup.birth %>% 
